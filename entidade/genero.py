1d10a55973f4ba51ca496f3373370a098fc05e55
gherrerar <gabi.gh597@gmail.com>
2023-10-04 13:21:02 -0300

    feat: Entidade Gênero

---
+++ entidade/genero.py
@@ -0,0 +1,16 @@
+
+
+class Genero:
+    def __init__(self, nome: str):
+        self.__nome = None
+        if isinstance(nome, str):
+            self.__nome = nome
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome: str):
+        if isinstance(nome, str):
+            self.__nome = nome
