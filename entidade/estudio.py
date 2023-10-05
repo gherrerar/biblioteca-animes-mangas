9b0d7420d66060a24c026a68d313f5eba1318aeb
João Pedro <joao_p_silveira@estudante.sc.senai.br>
2023-10-05 14:48:07 -0300

    feat: Entidade Estúdio

---
+++ entidade/estudio.py
@@ -0,0 +1,22 @@
+from anime import Anime
+
+
+class Estudio:
+    def __init__(self, nome: str):
+        self.__nome = None
+        self.__animes_produzidos = []
+        if isinstance(nome, str):
+            self.__nome = nome
+
+    @property
+    def nome(self) -> str:
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome: str):
+        if isinstance(nome, str):
+            self.__nome = nome
+
+    def add_anime(self, anime: Anime):
+        if isinstance(anime, Anime):
+            self.__animes_produzidos.append(anime)
