dacebdf92bbb3432d4e7531bcdc5c235ce554f97
João Pedro <joao_p_silveira@estudante.sc.senai.br>
2023-10-05 12:59:00 -0300

    feat: Entidade ExemplarManga

---
+++ entidade/exemplarManga.py
@@ -0,0 +1,19 @@
+from exemplar import Estado, Exemplar
+from manga import Manga
+
+
+class ExemplarManga(Exemplar):
+    def __init__(self, manga: Manga, etiqueta: Estado):
+        super().__init__(etiqueta)
+        self.__manga = None
+        if isinstance(manga, Manga):
+            self.__manga = manga
+
+    @property
+    def manga(self) -> Manga:
+        return self.__manga
+
+    @manga.setter
+    def manga(self, manga: Manga):
+        if isinstance(manga, Manga):
+            self.__manga = manga
