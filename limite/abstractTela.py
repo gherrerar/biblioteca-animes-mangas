4977c65f6600a1547e9053dacb2c1b8c2e466fff
gherrerar <gabi.gh597@gmail.com>
2023-10-14 20:11:49 -0300

    feat: Tratando entradas de int e string

--- limite/abstractTela.py
+++ limite/abstractTela.py
@@ -6,18 +6,56 @@ class AbstractTela(ABC):
     def __init__(self):
         pass
 
-    def le_num_inteiro(self, mensagem: str = "", nums_validos: [] = None) -> int:
+    def le_num_inteiro(self,
+                       mensagem: str = "",
+                       nums_validos: [] = None,
+                       minimo: int = None,
+                       maximo: int = None) -> int:
         while True:
-            valor = input(mensagem)
             try:
+                try:
+                    valor = input(mensagem)
+                except Exception as e:
+                    self.mostra_mensagem(type(e))
+
                 inteiro = int(valor)
                 if nums_validos and inteiro not in nums_validos:
+                    raise ValueError
+                if (minimo and inteiro < minimo) or (maximo and inteiro > maximo):
                     raise ValueError
                 return inteiro
             except ValueError:
                 self.mostra_mensagem("Valor incorreto! Digite um número inteiro válido")
                 if nums_validos:
                     self.mostra_mensagem(">>> Valores válidos: ", *nums_validos)
+                if minimo:
+                    self.mostra_mensagem(">>> Mínimo: ", minimo)
+                if maximo:
+                    self.mostra_mensagem(">>> Máximo: ", maximo)
+            except KeyboardInterrupt:
+                self.mostra_mensagem("\nEncerrando o sistema...")
+                exit(0)
+            except:
+                self.mostra_mensagem("Ocorreu um erro")
+
+    def le_texto(self, mensagem: str = "") -> str:
+        while True:
+            try:
+                try:
+                    texto = input(mensagem)
+                except Exception as e:
+                    self.mostra_mensagem(type(e))
+
+                if not texto or texto.isspace():
+                    raise ValueError
+                return texto.strip()
+            except ValueError:
+                self.mostra_mensagem("Valor incorreto! Insira um texto não vazio")
+            except KeyboardInterrupt:
+                self.mostra_mensagem("\nEncerrando o sistema...")
+                exit(0)
+            except:
+                self.mostra_mensagem("Ocorreu um erro")
 
     def mostra_mensagem(self, *msg: str):
         print(*msg)
