f870b3a7b7a2a5b3d110cb88bc4761f1c900c407
João Pedro <joao_p_silveira@estudante.sc.senai.br>
2023-10-15 16:44:30 -0300

    feat(Autor): Controlador e tela

---
+++ controle/ctrlAutor.py
@@ -0,0 +1,78 @@
+from controle.abstractCtrl import AbstractCtrl
+from limite.telaAutor import TelaAutor
+from entidade.autor import Autor
+
+
+class CtrlAutor(AbstractCtrl):
+    def __init__(self, ctrl_principal):
+        self.__autores = []
+        self.__tela_autor = TelaAutor()
+        super().__init__(ctrl_principal)
+
+    def abrir_tela(self):
+        opcoes = {
+            1: self.listar_autores,
+            2: self.incluir_autor,
+            3: self.incluir_manga,
+            0: self.retornar
+        }
+    
+        while True:
+            opcoes[self.__tela_autor.mostra_opcoes()]()
+
+    def listar_autores(self):
+        if self.__autores:
+            for autor in self.__autores:
+                self.__tela_autor.mostra_autor({
+                    'nome': autor.nome,
+                    'mangas': ', '.join(manga.titulo for manga in autor.mangas_produzidos)
+                })
+        else:
+            self.__tela_autor.mostra_autor(None)
+
+    def incluir_autor(self):
+        nome_autor = self.__tela_autor.recolhe_dados_autor()
+        if self.find_autor_by_nome(nome_autor) != None:
+            self.__tela_autor.mostra_mensagem("Atencao! Este autor ja existe!\n")
+        else:
+            autor = Autor(nome_autor)
+            self.__autores.append(autor)
+            self.__tela_autor.mostra_mensagem("Autor cadastrado!")
+
+    def incluir_manga(self):
+        autor = self.__existe_autor()
+        manga = self.__existe_manga()
+        if manga in autor.mangas_produzidos:
+            self.__tela_autor.mostra_mensagem("Atencao! Este autor e manga ja estao associados!")
+        else:
+            autor.add_manga(manga)
+            self.__tela_autor.mostra_mensagem("Autor e manga associados!")
+
+    def __existe_autor(self):
+        self.listar_autores()
+        while True:
+            nome_autor = self.__tela_autor.seleciona_autor()
+            autor = self.find_autor_by_nome(nome_autor)
+            if autor != None:
+                return autor
+            else:
+                self.__tela_autor.mostra_mensagem("Atencao! Este autor nao existe!\n")
+    
+    def __existe_manga(self):        
+        ctrl_manga = self.ctrl_principal.ctrl_manga
+        ctrl_manga.listar_mangas()
+        while True:
+            titulo_manga = self.__tela_autor.seleciona_manga()
+            manga = ctrl_manga.find_manga_by_titulo(titulo_manga)
+            if manga != None:
+                return manga
+            else:
+                self.__tela_autor.mostra_mensagem("Atencao! Este manga nao existe!\n")
+
+    def find_autor_by_nome(self, nome: str) -> Autor | None:
+        if self.__autores and isinstance(nome, str):
+            for autor in self.__autores:
+                if autor.nome == nome:
+                    return autor
+            return None
+
---
+++ limite/telaAutor.py
@@ -0,0 +1,39 @@
+from limite.abstractTela import AbstractTela
+
+
+class TelaAutor(AbstractTela):
+    def __init__(self):
+        pass
+
+    def mostra_opcoes(self) -> int:
+        print(f"{'AUTOR':-^30}")
+        print("1 - Listar")
+        print("2 - Cadastrar")
+        print("3 - Incluir Manga")
+        print("0 - Voltar")
+
+        opcao = super().le_num_inteiro("Escolha uma opcao: ", range(4))
+        return opcao
+
+    def mostra_autor(self, dados_autor: {}):
+        if not dados_autor:
+            self.mostra_mensagem("Nenhum autor foi cadastrado!")
+        else:
+            print("NOME:", dados_autor['nome'])
+            print("MANGAS:", dados_autor['mangas'])
+            print()
+
+    def recolhe_dados_autor(self) -> str:
+        print(f"{'DADOS AUTOR':-^30}")
+        nome = super().le_texto("Nome: ")
+        return nome
+
+    def seleciona_autor(self) -> str:
+        print(f"{'SELECIONA AUTOR':-^30}")
+        nome = super().le_texto("Nome: ")
+        return nome
+
+    def seleciona_manga(self) -> str:
+        print(f"{'SELECIONA MANGA':-^30}")
+        titulo = super().le_texto("Titulo: ")
+        return titulo
