5a88138e8bed136e06f3f6da6f869589abadfcf3
João Pedro <joao_p_silveira@estudante.sc.senai.br>
2023-11-23 11:53:24 -0300

    feat: Exceção para existência ou inexistência de objetos

---
+++ exceptions/existenceException.py
@@ -0,0 +1,6 @@
+
+
+class ExistenceException(Exception):
+    def __init__(self, classe: str, exists = True):
+        status = "ja existe" if exists else "nao existe"
+        super().__init__(f"Atencao! Este {classe} {status}!\n")
