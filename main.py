bb5e8d95130b5c6b193c7866facb52f2690aa601
gherrerar <gabi.gh597@gmail.com>
2023-11-25 00:53:19 -0300

    Acomodando o ambiente para interface gráfica

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ import sys, os
 
 if __name__ == "__main__":
     sys.path.insert(0, os.path.abspath(os.curdir))
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
     CtrlPrincipal().inicializar_sistema()
